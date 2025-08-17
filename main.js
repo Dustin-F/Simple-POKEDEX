f18f5de25ac1c614cc151dd317d0598b91db9ba1
Dusty <99244375+Dustin-F@users.noreply.github.com>
2025-08-17 14:04:44 +0800

    initial changes

--- main.js
+++ main.js
@@ -1,17 +1,20 @@
-document.querySelector("button").addEventListener("click", fetchPokemon);
+document.querySelector('button').addEventListener('click', fetchPokemon);
 
 function fetchPokemon() {
-  let pokemon = document.querySelector("input").value.toLowerCase();
+  let pokemon = document.querySelector('input').value.toLowerCase();
 
   fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}?offset=500&limit=500`)
     .then((res) => res.json()) // parse response as JSON
     .then((data) => {
       console.log(data);
 
-      document.querySelector("h2").innerText = data.name;
-      document.querySelector("img").src = data.sprites.front_shiny;
-      document.querySelector("h3").innerText = data.types[0].type.name;
+      document.querySelector('h2').innerText = data.name;
+      document.querySelector('img').src = data.sprites.front_shiny;
+      document.querySelector('h3').innerText = data.types[0].type.name;
+      document.querySelector('h4').innerText = data.abilities[0].ability.name;
+      document.querySelector('h5').innerText = data.abilities[1].ability.name;
     })
+
     .catch((err) => {
       console.log(`error ${err}`);
     });
